f8d0a5988d9147a18122a6740471a21988005991
Santiago Gimeno <santiago.gimeno@gmail.com>
2016-06-22 00:04:40 +0200

    test: fix test-fs-watch-encoding

--- test/parallel/test-fs-watch-encoding.js
+++ test/parallel/test-fs-watch-encoding.js
@@ -10,6 +10,20 @@ if (common.isFreeBSD) {
   return;
 }
 
+var files = [];
+var filesHex = [];
+var filesBuffer = [];
+
+try {
+  files = fs.readdirSync(common.tmpDir);
+  filesHex = fs.readdirSync(common.tmpDir, { encoding: 'hex' });
+  filesBuffer = fs.readdirSync(common.tmpDir, { encoding: 'buffer' });
+} catch (e) {
+  assert.equal(e.code, 'ENOENT');
+}
+
+common.refreshTmpDir();
+
 const fn = '新建文夹件.txt';
 const a = path.join(common.tmpDir, fn);
 
@@ -17,7 +31,7 @@ const watcher1 = fs.watch(
   common.tmpDir,
   {encoding: 'hex'},
   (event, filename) => {
-    if (filename)
+    if (filename && (filesHex.indexOf(filename) === -1))
       assert.equal(filename, 'e696b0e5bbbae69687e5a4b9e4bbb62e747874');
     watcher1.close();
   }
@@ -26,7 +40,7 @@ const watcher1 = fs.watch(
 const watcher2 = fs.watch(
   common.tmpDir,
   (event, filename) => {
-    if (filename)
+    if (filename && (files.indexOf(filename) === -1))
       assert.equal(filename, fn);
     watcher2.close();
   }
@@ -36,7 +50,7 @@ const watcher3 = fs.watch(
   common.tmpDir,
   {encoding: 'buffer'},
   (event, filename) => {
-    if (filename) {
+    if (filename && (filesBuffer.indexOf(filename) === -1)) {
       assert(filename instanceof Buffer);
       assert.equal(filename.toString('utf8'), fn);
     }
