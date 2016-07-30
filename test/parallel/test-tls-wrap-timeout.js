3df3f47dc73db51451c5386634ffa7a03634c788
Santiago Gimeno <santiago.gimeno@gmail.com>
2016-07-30 13:19:24 +0200

    Lets see

--- test/parallel/test-tls-wrap-timeout.js
+++ test/parallel/test-tls-wrap-timeout.js
@@ -16,27 +16,29 @@ var options = {
   cert: fs.readFileSync(common.fixturesDir + '/keys/agent1-cert.pem')
 };
 
-var server = tls.createServer(options, function(c) {
-  setTimeout(function() {
-    c.write('hello');
-    setTimeout(function() {
-      c.destroy();
-      server.close();
-    }, 150);
-  }, 150);
-});
+var server = tls.createServer(options, common.mustCall((c) => {
+  console.log('Server connected');
+}));
 
-server.listen(0, function() {
-  var socket = net.connect(this.address().port, function() {
-    var s = socket.setTimeout(common.platformTimeout(240), function() {
-      throw new Error('timeout');
-    });
-    assert.ok(s instanceof net.Socket);
-
+server.listen(0, common.mustCall(() => {
+  var socket = net.connect(server.address().port, function() {
+    console.log('Client connected');
     var tsocket = tls.connect({
       socket: socket,
       rejectUnauthorized: false
     });
     tsocket.resume();
+    setTimeout(common.mustCall(() => {
+      tsocket.write('hello');
+      setTimeout(common.mustCall(() => {
+        tsocket.destroy();
+        server.close();
+      }), common.platformTimeout(150));
+    }), common.platformTimeout(150));
+
+    var s = socket.setTimeout(common.platformTimeout(240), function() {
+      throw new Error('timeout');
+    });
+    assert.ok(s instanceof net.Socket);
   });
-});
+}));
