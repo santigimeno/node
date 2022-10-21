250a17a77fcaa2c7dca3c2b7a08d3666003636fe
Santiago Gimeno <santiago.gimeno@gmail.com>
2022-10-21 20:58:38 +0200

    test

--- test/parallel/test-http2-server-sessionerror.js
+++ test/parallel/test-http2-server-sessionerror.js
@@ -53,10 +53,12 @@ server.listen(0, common.mustCall(() => {
     .on('close', () => {
       server.removeAllListeners('error');
       http2.connect(url)
-        .on('error', common.expectsError({
-          code: 'ERR_HTTP2_SESSION_ERROR',
-          message: 'Session closed with error code 2',
-        }))
+      .on('error', common.mustCall((err) => {
+        if (err.code !== 'ECONNRESET') {
+          assert.strictEqual(err.code, 'ERR_HTTP2_SESSION_ERROR');
+          assert.strictEqual(err.message, 'Session closed with error code 2');
+        }
+      }))
         .on('close', () => server.close());
     });
 }));
